a7214d0ad76cce7023a16f4552b63dc45c230052
kevinfst <kevinst2610@gmail.com>
2025-02-18 20:20:28 -0300

    Criacao do Servidor

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,17 @@
-const a = 10
-const b = 5
-const result = a + b
+import http from "node:http";
 
-console.log("Hello World!")
-console.log(`Resultado: ${result}`)
+const server = http.createServer((request, response) => {
+    console.log("Servidor recebeu uma requisição");
+    response.end("Hello World!");
+});
+
+server.listen(3333, () => {
+    console.log("Servidor inicializado");
+});
+
+// Ao clicar Ctrl + C irá exibir a mensagem de Servidor finalizado
+// SIGINT significa "Signal Interrupt" que seria o CTRL + C, pois ele "interrompe" o server. Seria um dos sinais do sistema Unix/Linux, ele é enviado para um processo ao pressionar CTRL + C no terminal.
+process.on("SIGINT", () => {
+    console.log("Servidor finalizado");
+    process.exit();
+});
